be75556bb882275fc79a47c79bbc8c099123511f
Stanislav <brickbybrick15@gmail.com>
2023-10-03 19:21:49 +0300

    commit
    commit

--- src/1.ts
+++ src/1.ts
@@ -1,17 +1,17 @@
-/*
-  Клас Student, який містить три властивості: name, age та grade. 
-  Замість того, щоб оголошувати ці властивості в тілі класу, потім у конструкторі, і нарешті надавати їм значення, 
-  напишіть скорочену ініціалізацію.
-*/
+// /*
+//   Клас Student, який містить три властивості: name, age та grade. 
+//   Замість того, щоб оголошувати ці властивості в тілі класу, потім у конструкторі, і нарешті надавати їм значення, 
+//   напишіть скорочену ініціалізацію.
+// */
 
-class Student {
-  constructor(public name: string='Stas', public age: number=26, public grade: string='masters') {
+// class Student {
+//   constructor(public name: string='Stas', public age: number=26, public grade: string='masters') {
     
-  }
-}
+//   }
+// }
 
-console.log(Student)
+// console.log(Student)
 
 
 
-export {};
+// export {};
